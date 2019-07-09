715b4923a20905647a3d60efea062ca50f64adb0
Jannis Achstetter <jannis.achstetter@schneider-electric.com>
2019-07-09 12:02:04 +0200

    Add XCode project files to build iOS frameworkds Add arch to build script, add config.h and modbus-version.h to requried files and define required headers as public ones Add config.h for iOS Add iOS and XCode specific files to .gitignore config.h.ios.ion: Reflect reality that iOS cannot have linux/serial.h

---
+++ libmodbus/libmodbus/libmodbus.h
@@ -0,0 +1,13 @@
+//
+//  libmodbus.h
+//  libmodbus
+//
+//  Created by Schneider Electric on 19.03.18.
+//  Copyright © 2018 schneiderelectric. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libmodbus : NSObject
+
+@end
---
+++ libmodbus/libmodbus/libmodbus.m
@@ -0,0 +1,13 @@
+//
+//  libmodbus.m
+//  libmodbus
+//
+//  Created by Schneider Electric on 19.03.18.
+//  Copyright © 2018 schneiderelectric. All rights reserved.
+//
+
+#import "libmodbus.h"
+
+@implementation libmodbus
+
+@end
